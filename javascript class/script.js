dbeec3ab3ab22fcd570086411e959f207fb9947d
obinna <obinnaowunna8@gmail.com>
2026-05-21 13:41:29 +0100

    first commit

---
+++ javascript class/script.js
@@ -0,0 +1,16 @@
+// javascript
+// variable
+// types of variable
+// variable names
+// rules for naming variable namedatatypes
+let myName= 'Obinna';
+console.log(myName);
+//  function myFruit(myNam){
+// if(MyNam !="Obinna"){
+//     console.log("this is not correct")
+// }
+// else{
+//     console.log("this is" + myNam )
+// }
+// }
+// console.log(myFruit)

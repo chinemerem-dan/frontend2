8ba488e342af4393b291926a39259640c1490839
obinna <obinnaowunna8@gmail.com>
2026-05-22 11:48:02 +0100

    made changes in my calulator

--- CSS CLASS/card.js
+++ CSS CLASS/card.js
@@ -17,19 +17,28 @@ buttons.forEach(button => {
     else if (value === "DEL") {
       display.value = display.value.slice(0, -1);
     }
+     else if(value === "%"){
+      display.value = eval(display.value/100)
+    }
 
+    
     // Calculate result
     else if (value === "=") {
 
       try {
-        display.value = eval(display.value);
+        const expression = display.value.replace(/\^/g, "**");
+         display.value= eval(display.value)
+
+       display.value= eval(expression)
       }
+
 
       catch {
         display.value = "Error";
       }
 
     }
+   
 
     // Append values
     else {
